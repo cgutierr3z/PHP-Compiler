31d86c465f820a8ed899865a9cf3efb0e27befbe
z3774 <rapsta93@gmail.com>
2016-05-17 20:46:43 -0500

    solved error

--- php_parser.py
+++ php_parser.py
@@ -20,284 +20,298 @@ from php_lexer import tokens
 VERBOSE = 1
 
 precedence = (
-    ('left', 'INCLUDE', 'INCLUDE_ONCE', 'EVAL', 'REQUIRE', 'REQUIRE_ONCE'),
+    ('left', 'INCLUDE', 'REQUIRE'),
     ('left', 'COMMA'),
-    ('right', 'PRINT'),
     ('left', 'EQUAL', 'PLUSEQUAL', 'MINUSEQUAL'),
-    ('left', 'COLON'),
+    ('left', 'SEMI'),
     ('left', 'OR'),
     ('left', 'XOR'),
     ('left', 'AND'),
     ('nonassoc', 'ISEQUAL', 'DEQUAL'),
     ('nonassoc', 'LESS', 'LESSEQUAL', 'GREATER', 'GREATEREQUAL'),
     ('left', 'PLUS', 'MINUS'),
-    ('nonassoc', 'INSTANCEOF'),
     ('right', 'LBRACKET'),
     ('nonassoc', 'NEW', 'CLONE'),
-    #('left', 'ELSEIF'),
-    #('left', 'ELSE'),
-    #('left', 'ENDIF'),
-    ('right', 'STATIC', 'ABSTRACT', 'PRIVATE', 'PROTECTED', 'PUBLIC'),
+    ('left', 'ELSEIF'),
+    ('left', 'ELSE'),
+    ('right', 'PRIVATE', 'PROTECTED', 'PUBLIC'),
 )
 
 def p_program(p):
-    'program : declaration_list'
+    'program : OPENTAG declaration_list CLOSETAG'
     pass
 
 def p_declaration_list(p):
-    '''declaration_list : OPENTAG declaration_list declaration CLOSETAG declaration_list
-                        | declaration'''
-    pass
+   '''declaration_list : declaration_list  declaration
+   					   | declaration
+   '''
+   pass
 
 def p_declaration(p):
-    '''declaration : statement
-                   | function_declaration_statement
-                   | class_declaration_statement
-                   | empty'''
-    pass
+	'''declaration : var_declaration
+				   | fun_declaration
+				   | area fun_declaration
+				   | header_declaration
+				   | class_declaration
+				   | echo_stmt
+				   | selection_stmt
+			       | iteration_stmt
+				   | typeclass
+	'''
+	pass
 
-def p_statement(p):
-    'statement : expr SEMI'
-    pass
+def p_echo_stmt(p):
+	'''echo_stmt : echo_stmt ECHO STRING SEMI
+				 | echo_stmt ECHO IDVAR SEMI
+				 | empty
+				 | echo_stmt ECHO NUM SEMI
+				 | echo_stmt ECHO boolean SEMI
+				 | echo_stmt ECHO fun_declaration SEMI
+	'''
+	pass
 
-def p_statement_if(p):
-    '''statement : IF LPAREN expr RPAREN LBLOCK statement RBLOCK elseif_list else_single
-                 | IF LPAREN expr RPAREN statement'''
-    pass
+def p_header_declaration(p):
+	'''header_declaration : REQUIRE LPAREN STRING RPAREN SEMI
+                          | INCLUDE LPAREN STRING RPAREN SEMI
+    '''
+	pass
 
-def p_elseif_list(p):
-    '''elseif_list : empty
-                   | elseif_list ELSEIF LPAREN expr RPAREN statement
-                   | elseif_list ELSEIF LPAREN expr RPAREN LBLOCK statement RBLOCK'''
-    pass
+def p_class_declaration(p):
+	'''class_declaration : area CLASS ID LBLOCK attribute RBLOCK
+						 | CLASS ID LBLOCK attribute RBLOCK
+	'''
+	pass
 
-def p_else_single(p):
-    '''else_single : empty
-                   | ELSE statement
-                   | ELSE LBLOCK statement RBLOCK'''
-    pass
-
-def p_statement_while(p):
-    'statement : WHILE LPAREN expr RPAREN while_statement'
-    pass
-
-def p_while_statement(p):
-    'while_statement : statement'
-    pass
-
-def p_statement_do_while(p):
-    'statement : DO statement WHILE LPAREN expr RPAREN SEMI'
-    pass
-
-
-def p_statement_for(p):
-    'statement : FOR LPAREN for_expr SEMI for_expr SEMI for_expr RPAREN for_statement'
-    pass
-
-def p_for_expr(p):
-    '''for_expr : empty
-                | non_empty_for_expr'''
-    pass
-
-def p_non_empty_for_expr(p):
-    '''non_empty_for_expr : non_empty_for_expr COMMA expr
-                          | expr'''
-    pass
-
-def p_for_statement(p):
-    'for_statement : statement'
-    pass
-
-def p_statement_break(p):
-    '''statement : BREAK SEMI
-                 | BREAK expr SEMI'''
-    pass
-
-def p_statement_continue(p):
-    '''statement : CONTINUE SEMI
-                 | CONTINUE expr SEMI'''
-    pass
-
-def p_statement_return(p):
-    '''statement : RETURN SEMI
-                 | RETURN expr SEMI'''
-    pass
-
-def p_statement_global(p):
-    'statement : GLOBAL global_var_list SEMI'
-    pass
-
-def p_global_var_list(p):
-    '''global_var_list : global_var_list COMMA global_var
-                       | global_var'''
-    pass
-
-def p_global_var(p):
-    'global_var : IDVAR'
-    pass
-
-def p_statement_static(p):
-    'statement : STATIC static_var_list SEMI'
-    pass
-
-def p_static_var_list(p):
-    '''static_var_list : static_var_list COMMA static_var
-                       | static_var'''
-    pass
-
-def p_static_var(p):
-    '''static_var : IDVAR EQUAL NUM
-                  | IDVAR EQUAL STRING
-                  | IDVAR EQUAL ID
-                  | IDVAR'''
-    pass
-
-def p_statement_echo(p):
-    'statement : ECHO echo_expr_list SEMI'
-    pass
-
-def p_echo_expr_list(p):
-    '''echo_expr_list : echo_expr_list COMMA expr
-                      | expr '''
-    pass
+def p_attribute1(p):
+	'''attribute : attribute area var_declaration
+				 | area var_declaration
+				 | attribute area fun_declaration
+				 | area fun_declaration
+	'''
+	pass
 
 
-def p_function_declaration_statement(p):
-    'function_declaration_statement : FUNCTION ID LPAREN parameter_list RPAREN LBLOCK declaration RBLOCK'
-    pass
+def p_area(p):
+	'''area : PRIVATE
+			| PUBLIC
+			| PROTECTED
+	'''
+	pass
 
-def p_class_declaration_statement(p):
-    'class_declaration_statement : class_entry_type ID LBLOCK class_statement_list RBLOCK'
-    pass
+def p_var_declaration(p):
+	'''var_declaration : IDVAR SEMI var_declaration
+                       | IDVAR SEMI
+                       | TIMESTIMES IDVAR SEMI
+                       | TIMESTIMES IDVAR SEMI var_declaration
+                       | IDVAR EQUAL NUM SEMI var_declaration
+                       | IDVAR EQUAL NUM SEMI
+                       | IDVAR EQUAL boolean SEMI var_declaration
+                       | IDVAR EQUAL boolean SEMI
+                       | IDVAR EQUAL IDVAR SEMI var_declaration
+                       | IDVAR EQUAL IDVAR SEMI
+                       | AMPERSANT IDVAR SEMI var_declaration
+                       | AMPERSANT IDVAR EQUAL IDVAR SEMI  selection_stmt
+                       | AMPERSANT IDVAR SEMI
+                       | IDVAR EQUAL simple_expression SEMI
+	'''
+	pass
 
-def p_class_entry_type(p):
-    '''class_entry_type : CLASS
-                        | PROTECTED CLASS'''
-    pass
+def p_fun_declaration(p): #declarando funciones
+	'''fun_declaration : FUNCTION ID LPAREN params RPAREN
+					   | FUNCTION ID LPAREN params RPAREN compount_stmt
+	'''
+	pass
 
-def p_class_statement_list(p):
-    '''class_statement_list : class_statement_list class_statement
-                            | empty'''
-    pass
+def p_params(p):
+	'''params : param_list
+			  | empty
+	'''
+	pass
 
-def p_class_statement(p):
-    '''class_statement : FUNCTION ID LPAREN parameter_list RPAREN method_body
-                       | class_variable_declaration SEMI
-                       | class_constant_declaration SEMI'''
-    pass
+def p_param_list(p):#variabaleb, variables, etc
+	'''param_list : param_list COMMA param_list
+				  | param
+	'''
+	pass
 
-def p_class_variable_declaration(p):
-    '''class_variable_declaration : class_variable_declaration COMMA IDVAR EQUAL static_scalar
-                                  | IDVAR EQUAL static_scalar'''
-    pass
-
-def p_class_constant_declaration(p):
-    '''class_constant_declaration : class_constant_declaration COMMA STRING EQUAL static_scalar
-                                  | CONST IDVAR EQUAL static_scalar'''
-    pass
+def p_param(p):
+	'''param : IDVAR
+             | IDVAR LBRACKET RBRACKET
+    '''
+	pass
 
 
-def p_method_body(p):
-    '''method_body : LBLOCK declaration RBLOCK
-                   | SEMI'''
-    pass
+def p_compount_stmt(p):#{argumento}
+	'compount_stmt : LBLOCK echo_stmt local_declarations echo_stmt statement_list echo_stmt RBLOCK'
+	pass
 
-def p_parameter_list(p):
-    '''parameter_list : parameter_list COMMA parameter
-                      | parameter'''
-    pass
+def p_local_declarations(p):#recursividad por la izquierda
+	'''local_declarations : local_declarations var_declaration
+						  | empty
+	'''
+	pass
 
-def p_parameter(p):
-    'parameter : IDVAR'
-    pass
+def p_statement_list(p):
+	'''statement_list : statement_list statement
+					  | empty
+	'''
+	pass
 
-def p_expr_variable(p):
-    'expr : variable'
-    pass
+def p_statement(p):#declarando sentencias
+	'''statement : expression_stmt
+				 | compount_stmt
+				 | selection_stmt
+				 | iteration_stmt
+			     | return_stmt
+			     | class_declaration
+				 | echo_stmt
+	'''
+	pass
 
-def p_expr_assign(p):
-    '''expr : IDVAR EQUAL NUM
-            | IDVAR EQUAL STRING
-            | IDVAR EQUAL IDVAR'''
-    pass
+def p_expression_stmt(p):#IDVAR;
+	'expression_stmt : expression SEMI'
+	pass
 
-def p_variable(p):
-    '''variable : IDVAR
-                | function_call'''
-    pass
 
-def p_function_call(p):
-    'function_call : ID LPAREN function_call_parameter_list RPAREN'
-    pass
+def p_selection_stmt_1(p):
+	'''selection_stmt : IF LPAREN expression RPAREN statement
+					  | IF LPAREN expression RPAREN statement selection
 
-def p_expr_scalar(p):
-    'expr : scalar'
-    pass
+	'''
+	pass
 
-def p_scalar(p):
-    '''scalar : NUM
-              | STRING'''
-    pass
+def p_selection(p):
+	'''selection : ELSE statement
+				 | ELSEIF statement
+	 '''
+	pass
 
-def p_function_call_parameter_list(p):
-    '''function_call_parameter_list : function_call_parameter_list COMMA function_call_parameter
-                                    | function_call_parameter'''
-    pass
+def p_selection_stmt_2(p):
+	'''selection_stmt : SWITCH LPAREN var RPAREN statement
+					  | CASE NUM COLON statement BREAK SEMI
+					  | DEFAULT COLON statement BREAK SEMI
 
-def p_function_call_parameter_list_empty(p):
-    'function_call_parameter_list : empty'
-    pass
+	'''
+	pass
 
-def p_function_call_parameter(p):
-    'function_call_parameter : expr'
-    pass
+def p_iteration_stmt_1(p):
+	'iteration_stmt : FOR LPAREN var_declaration SEMI expression SEMI additive_expression RPAREN statement '
+	pass
+def p_iteration_stmt_2(p):
+	'iteration_stmt : WHILE LPAREN expression RPAREN statement'
+	pass
 
-def p_expr_function(p):
-    'expr : FUNCTION LPAREN parameter_list RPAREN'
-    pass
+def p_iteration_stmt_3(p):
+	'iteration_stmt : DO LBLOCK statement SEMI RBLOCK WHILE LPAREN expression RPAREN'
+	pass
 
-def p_expr_assign_op(p):
-    '''expr : variable PLUSEQUAL expr
-            | variable MINUSEQUAL expr
-            | variable LESSEQUAL expr
-            | variable GREATEREQUAL expr'''
-    pass
 
-def p_expr_binary_op(p):
-    '''expr : expr AND expr
-            | expr ISEQUAL expr
-            | expr OR expr
-            | expr GREATER expr
-            | expr LESS expr
-            | expr XOR expr
-            | expr PLUS expr
-            | expr MINUS expr
-            | expr TIMES expr
-            | expr DIVIDE expr'''
-    pass
+def p_return_stmt(p):
+	'''return_stmt : RETURN SEMI
+				   | RETURN expression SEMI
+	'''
+	pass
 
-def p_expr_unary_op(p):
-    '''expr : PLUS expr
-            | MINUS expr'''
-    pass
+def p_expression(p):
+	'''expression : var EQUAL expression
+				  | simple_expression
+				  | var EQUAL AMPERSANT IDVAR
+			      | expression AND expression
+				  | expression OR expression
+	'''
+	pass
 
-def p_expr_print(p):
-    'expr : PRINT expr'
-    pass
+def p_var(p):
+	'''var : IDVAR
+		   | IDVAR LBRACKET expression RBRACKET
+	'''
+	pass
 
-def p_expr_group(p):
-    'expr : LPAREN expr RPAREN'
-    pass
 
-def p_static_scalar(p):
-    '''static_scalar : NUM
-                     | STRING'''
+def p_simple_expression(p):
+	'''simple_expression : additive_expression relop additive_expression
+						 | additive_expression
+	'''
+	pass
 
-    pass
+def p_relop(p):
+	'''relop : LESS
+			 | LESSEQUAL
+			 | GREATER
+			 | GREATEREQUAL
+			 | DEQUAL
+			 | DISTINT
+			 | ISEQUAL
+	'''
+	pass
+
+def p_additive_expression(p):
+	'''additive_expression : additive_expression addop term
+    					   | term
+    					   | term MINUSMINUS
+    				       | term PLUSPLUS
+	'''
+	pass
+
+def p_addop(p):
+	'''addop : PLUS
+			 | MINUS
+	'''
+	pass
+
+def p_term(p):
+	'''term : term mulop factor
+			| factor
+	'''
+	pass
+
+def p_mulop(p):
+	'''mulop : TIMES
+			 | DIVIDE
+	'''
+	pass
+
+def p_factor(p):
+	'''factor : LPAREN expression RPAREN
+			  | var
+			  | NUM
+			  | boolean
+			  | IDVAR LPAREN args RPAREN
+	'''
+	pass
+
+def p_args(p):
+	'''args : args_list
+			| empty
+			| VOID
+	'''
+	pass
+
+def p_args_list(p):
+	'''args_list : args_list COMMA expression
+				 | expression
+	'''
+	pass
+
+def p_boolean(p):
+	'''boolean : TRUE
+			   | FALSE
+	'''
+	pass
+
+def p_tclass(p):
+	'typeclass : ID IDVAR EQUAL NEW constructor SEMI'
+	pass
+
+def p_costructor(p):
+	'''constructor : ID LPAREN RPAREN
+				   | ID LPAREN args RPAREN
+	'''
+	pass
 
 def p_empty(p):
-    'empty : '
-    pass
+	'empty :'
+	pass
 
 def p_error(p):
     if VERBOSE:
@@ -310,7 +324,6 @@ def p_error(p):
 
     else:
         raise Exception('syntax', 'error')
-
 
 parser = yacc.yacc()
 
